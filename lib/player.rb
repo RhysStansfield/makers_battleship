a0bc712dc9bb45b8a16381e7d1e3591c03467ef2
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-10-22 11:48:30 +0100

    Added player tests for creating board and checking ships

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,26 @@
+require 'board'
+
 class Player
-  def initalize name
-  	@player = player
-  	@board = board_creator
+
+  def initialize name
+  	@player = name
+  	@board = Board.new name
+    give_final_board
+  end
+
+  def give_final_board
+    @board.final_board
   end
 
   def has_ships_still_floating?
-
+    @board.board_layout.flatten.include?('s')
   end
 
   def shoot(at_coordinates, opponent_board)
+  end
+
+  def has_board?
+    !@board.nil?
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,15 +1,26 @@
 require 'player'
-require 'board'
+# require 'board'
 
 describe Player do
-  it 'cockadoodledo' do
 
-
-
+  it 'tells us if the player has a board when initialized' do
+    player = Player.new 'Jeff'
+    expect(player).to have_board
   end
 
-it 'tells us if the ship is still floating' do
-end
+
+
+  it 'tells us if the ship is still floating' do
+    board = double :board, board_layout: [[' ', ' ', 's']]
+    player = Player.new 'Jeff'
+    expect(player.has_ships_still_floating?).to be true
+  end
+
+  it 'tells us if the player has no ships still floating' do
+    board = double :board, board_layout: [[' ', ' ', ' ']]
+    player = Player.new 'Jeff'
+    expect(player.has_ships_still_floating?).to be false
+  end
 
 end
 
